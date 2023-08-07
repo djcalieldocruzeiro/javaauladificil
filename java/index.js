02b0803d3c7f42705800393ad3c139b33f6dabbb
djcalieldocruzeiro <139808773+djcalieldocruzeiro@users.noreply.github.com>
2023-08-07 16:24:11 -0300

    Add files via upload

---
+++ java/index.js
@@ -0,0 +1,15 @@
+var idade,eleitor,resultado;
+idade = 71;
+idade = 65;;
+eleitor = (idade<18);
+resultado = (idade===65) ||( idade ===10); // false or true
+alert (resultado);
+
+function Soma (a,b){
+    if (a+b ===50){
+        console.log(a+b)+ "é igual a 50");
+    }Else{
+        console.log((a+b)+ "Não e igual a 50");
+    }
+    }
+console.log(soma(10,10))
